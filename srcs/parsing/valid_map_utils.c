666cb3cc464fcb863855b4bdfc4cb0a244dcaa73
Jean SAUVAGE <jsauvage@e3r5p19.clusters.42paris.fr>
2023-02-21 14:27:09 +0100

    fix check extension file

--- srcs/parsing/valid_map_utils.c
+++ srcs/parsing/valid_map_utils.c
@@ -6,7 +6,7 @@
 /*   By: jsauvage <jsauvage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 14:34:07 by jsauvage          #+#    #+#             */
-/*   Updated: 2023/02/20 18:50:58 by jsauvage         ###   ########.fr       */
+/*   Updated: 2023/02/21 14:25:18 by jsauvage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,17 +30,15 @@ int	check_player_composant(int i)
 int	check_extension(char *file_name)
 {
 	char	*str;
-	int		i;
 
-	i = 0;
-	str = ft_strchr(file_name, '.');
+	if (file_name[0] == '.' && file_name[1] == 'c' && file_name[2] == 'u'
+		&& file_name[3] == 'b' && file_name[4] == '\0')
+		return (FALSE);
+	str = ft_strrchr(file_name, '.');
 	if (str == NULL)
 		return (FALSE);
 	if (str[0] == '.' && str[1] == 'c' && str[2] == 'u' && str[3] == 'b'
 		&& str[4] == '\0')
 		return (TRUE);
-	if (*str == '.')
-		str++;
-	check_extension(str);
 	return (FALSE);
 }
