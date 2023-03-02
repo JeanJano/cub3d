f699231adc213714996a5dce97e47146b7750f4b
Jean SAUVAGE <jsauvage@e3r5p19.clusters.42paris.fr>
2023-03-02 18:54:48 +0100

    working on move

--- srcs/event.c
+++ srcs/event.c
@@ -6,7 +6,7 @@
 /*   By: jsauvage <jsauvage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 13:21:25 by jsauvage          #+#    #+#             */
-/*   Updated: 2023/03/01 16:55:06 by jsauvage         ###   ########.fr       */
+/*   Updated: 2023/03/02 18:54:24 by jsauvage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,35 @@ void	move_key(int key, t_cub **cub)
 {
 	if (key == XK_w && (*cub)->parsing.map[(int)(*cub)->player_x + 1][(int)(*cub)->player_y] != 1)
 	{
-		// printf("map: %d\n", (*cub)->parsing.map[(int)(*cub)->player_x][(int)(*cub)->player_y]);
 		printf("w\n");
-		(*cub)->player_x = (*cub)->player_x + (cos((*cub)->vision) * (*cub)->move_incr);
-		(*cub)->player_y = (*cub)->player_y + (sin((*cub)->vision) * (*cub)->move_incr);
-		(*cub)->player_x = ceilf((*cub)->player_x * 100) / 100;
-		(*cub)->player_y = ceilf((*cub)->player_y * 100) / 100;
+		if ((*cub)->vision > 180 && (*cub)->vision < 270)
+		{
+			(*cub)->player_x -= cos(270 - (*cub)->vision) * (*cub)->move_incr;
+			(*cub)->player_y -= sin(270 - (*cub)->vision) * (*cub)->move_incr;
+		}
+		else if ((*cub)->vision > 270 && (*cub)->vision < 360)
+		{
+			(*cub)->player_x += cos((*cub)->vision - 270) * (*cub)->move_incr;
+			(*cub)->player_y += sin((*cub)->vision - 270) * (*cub)->move_incr;
+		}
+		else if ((*cub)->vision > 90 && (*cub)->vision < 180)
+		{
+			(*cub)->player_x += -cos((*cub)->vision - 90) * (*cub)->move_incr;
+			(*cub)->player_y += -sin((*cub)->vision - 90) * (*cub)->move_incr;
+		}
+		else if ((*cub)->vision > 0 && (*cub)->vision < 90)
+		{
+			(*cub)->player_x += cos(90 - (*cub)->vision) * (*cub)->move_incr;
+			(*cub)->player_y -= sin(90 - (*cub)->vision) * (*cub)->move_incr;
+		}
+		else if ((*cub)->vision == 270)
+			(*cub)->player_x += cos(270 - (*cub)->vision) * (*cub)->move_incr;
+		else if ((*cub)->vision == 90)
+			(*cub)->player_x -= cos(90 - (*cub)->vision) * (*cub)->move_incr;
+		else if ((*cub)->vision == 180)
+			(*cub)->player_y -= cos((*cub)->vision - 90) * (*cub)->move_incr; // a retravailler doit se deplacer de -0.2 dans y
+		// (*cub)->player_x = ceilf((*cub)->player_x * 100) / 100;
+		// (*cub)->player_y = ceilf((*cub)->player_y * 100) / 100;
 	}
 	// if (key == XK_s && (*cub)->parsing.map[(int)(*cub)->player_x - 1][(int)(*cub)->player_y] != 1)
 	// {
