cf1689a9e4182a71c01b923d8cf933ed3f0b82b3
Jean SAUVAGE <jsauvage@e3r5p19.clusters.42paris.fr>
2023-03-14 17:11:17 +0100

    floor and ceiling

--- srcs/projection/projection.c
+++ srcs/projection/projection.c
@@ -6,7 +6,7 @@
 /*   By: jsauvage <jsauvage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 18:53:06 by jsauvage          #+#    #+#             */
-/*   Updated: 2023/03/14 16:27:53 by jsauvage         ###   ########.fr       */
+/*   Updated: 2023/03/14 17:09:02 by jsauvage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,27 +30,54 @@ static void	draw_backgroud(t_cub **cub)
 	}
 }
 
+int	color(int *color_tab)
+{
+	int color;
+
+	color = color_tab[2];
+	color += color_tab[1] << 8;
+	color += color_tab[0] << 16;
+	return (color);
+}
+
 void	draw_line(t_cub **cub)
 {
 	int		i;
 	int		j;
 	double	angle;
 	double	wall_heigth;
+	int	incr;
 
 	i = 0;
 	angle = (*cub)->vision - 30;
-	printf("angle: %f\n", (*cub)->vision);
+	if (angle < 0)
+		angle += 360;
+	// printf("angle: %f\n", (*cub)->vision);
 	while (i < WINDOW_WIDTH)
 	{
 		(*cub)->distance = get_vector_distance((*cub)->player_x, (*cub)->player_y, angle, (*cub)->parsing);
 		// printf("distance: %f\n", (*cub)->distance);
-		wall_heigth = (10 / (*cub)->distance) * 350;
+		wall_heigth = (10 / (*cub)->distance) * 320;
 		// printf("wall_height: %f\n", wall_heigth);
 		j = (WINDOW_HEIGHT / 2) - ((int)wall_heigth / 2);
 		// printf("j: %d\n", j);
+		incr = 0;
+		while (incr < j)
+		{
+			img_pix_put(&(*cub)->mlx.img, i, incr, color((*cub)->parsing.rgb_plafond));
+			incr++;
+		}
 		while (j < (int)wall_heigth)
 		{
 			img_pix_put(&(*cub)->mlx.img, i, j, 0x00FF6F06);
+			j++;
+		}
+		// incr = 0;
+		j = wall_heigth;
+		// printf("%d\n", j);
+		while (j < WINDOW_HEIGHT)
+		{
+			img_pix_put(&(*cub)->mlx.img, i, j, color((*cub)->parsing.rgb_floor));
 			j++;
 		}
 		i++;
